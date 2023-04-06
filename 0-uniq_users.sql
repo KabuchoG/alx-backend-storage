9c20af069965cac194c41172a06a9ea67e8b80f7
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-06 11:00:55 -0700

    new commit

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- task one
 -- creates table users
-CREATE TABLE IF NOT EXISTS users (
+DROP IF EXISTS users;
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,

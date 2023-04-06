a6e7402734201c82304e6bed88daaa003913b0e4
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-06 10:54:31 -0700

    new commit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- creates table users
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );

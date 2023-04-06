12f69969be82f8151a50897055b03459d5b66334
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-06 11:22:08 -0700

    final updates

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- task one
 -- creates table users
-DROP IF EXISTS users;
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,

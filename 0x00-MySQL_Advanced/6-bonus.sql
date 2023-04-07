71f5cc30722b80248c7712827fa8c1a3a3d871f2
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-07 05:48:50 -0700

    new commit

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,5 +1,5 @@
 --  creates a stored procedure AddBonus that adds a new correction for a student.
-DROP PRICEDURE IF EXISTS AddBonus
+DROP PROCEDURE IF EXISTS AddBonus
 DELIMITER $$
 CREATE PROCEDURE AddBonus(
     IN user_id INT, 
@@ -7,12 +7,12 @@ CREATE PROCEDURE AddBonus(
     IN score INT)
     BEGIN
         DECLARE project_id INT;
-        SELECT id INTO project_id FROM projects WHERE name = project_name LIMIT 1;
-        IF project_id IS NULL 
+        IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
         THEN
-            INSERT INTO projects(name) VALUES(project_name);
-            SET project_id = LAST_INSERT_ID();
+        INSERT INTO projects(name) VALUES(project_name);
         END IF;
+            
+        SET project_id = (SELECT id FROM projects FROM projects WHERE name = project_name LIMIT 1);
         INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
-    END; $$
+    END $$
 DELIMITER ;

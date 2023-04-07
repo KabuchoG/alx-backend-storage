ee9692a07462ea03eb156ad832570daf89e4d6b9
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-07 00:42:17 -0700

    new commit

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
 -- list all band_name, lifespan (formed to split or current year)
-SELECT band_name, (IFNULL(split, YEAR(NOW())) - formed) AS lifespan
+SELECT band_name, (IFNULL((split - formed), 0) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;

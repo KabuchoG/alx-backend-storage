959a7792e5f00edb3cbf354c123c12fc60a9174f
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-14 08:00:00 -0700

    new commit

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,5 +5,5 @@
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic
     """
-    # return list(mongo_collection.find({'topics': {'$elemMatch': { '$eq': topic}}}, {'school': 1}))
-    return list(mongo_collection.find({'topics': topic}))
+    # return list(mongo_collection.find({'topics': topic}))
+    return list(mongo_collection.find({'topics': {'$elemMatch': {'$eq': topic}}}, {'school': 1}))
